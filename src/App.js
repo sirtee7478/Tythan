6a0e76e4138360eac43b8755e6f530d476166222
sirtee7478 <sirtee7478@gmail.com>
2023-02-01 18:52:40 +0100

    Nothing much

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-
 import CentralRouter from './CentralRouter';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -17,8 +16,8 @@ function App() {
         <CentralRouter />
         <Footer />
       </Router>
-  </div>
-  )
+    </div>
+  );
 }
 
 export default App;
